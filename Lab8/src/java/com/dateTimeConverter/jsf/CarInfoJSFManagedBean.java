e577a133f5ce4e01a3fe68281de23fc514d10bec
Bimal Parajuli <bimal.parajuli@ICIDigitals-MacBook-Pro-37.local>
2018-04-17 14:07:54 -0400

    Added validator for all car infos

--- Lab8/src/java/com/dateTimeConverter/jsf/CarInfoJSFManagedBean.java
+++ Lab8/src/java/com/dateTimeConverter/jsf/CarInfoJSFManagedBean.java
@@ -5,8 +5,12 @@
  */
 package com.dateTimeConverter.jsf;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 
 
 /**
@@ -43,7 +47,14 @@ public class CarInfoJSFManagedBean {
     public void setPrice(Long price) {
         this.price = price;
     }
-    
+    public void validateBrand(FacesContext context, UIComponent toValidate, Object value){
+        brand = (String) value;
+        if(!(brand.equalsIgnoreCase("BMW") || brand.equalsIgnoreCase("Mercedes"))){
+            ((UIInput) toValidate).setValid(false);
+            FacesMessage message = new FacesMessage("Invalid Brand");
+            context.addMessage(toValidate.getClientId(context), message);
+        }
+    }
     public String submit(){
         return null;
     }
